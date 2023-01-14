288fbc9208b7db26e8fc758cdf704d9c80cbb78a
CRYPTAN77 <sardorbaltaev8@gmail.com>
2023-01-14 18:06:33 +0500

    initial commit

---
+++ rebra.js
@@ -0,0 +1,235 @@
+// let a="sardor";
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a[5]);
+//  let a=423;
+
+// let first= Math.floor(a/100);
+//  let second=(a%100);
+
+// let third=a-513;
+//  let result=Number(""+second+first-third);
+//  console.log(result);
+// console.log(a)
+// let number =423;
+// let first =Math.floor(number/100);
+// let second=number%100;
+// let third=number-513;
+// let res=Number(""+second+first-third);
+// console.log(number);
+// console.log(res);
+// let mall,qoy,tauk,shoshqa;
+// mall = Number(prompt());
+// qoy = Number(prompt());
+// tauk = Number(prompt());
+// shoshqa = Number(prompt());
+// console.log(mall * 4);
+// console.log(qoy * 4);
+// console.log(tauk * 2);
+// console.log(shoshqa * 4);
+// let nums = Math.floor(Math.random()*10)
+// let random
+// if(nums%2==0){
+// random=Math.floor(Math.random()*20)+20;
+// }
+// else {random=Math.floor(Math.random()*60)+40;}
+// console.log(random)
+// let time= new Date()
+// let hour = time.getHours()
+// let minute = time.getMinutes()
+// let seconds = time.getSeconds()
+// console.log(seconds,minute,hour)
+// let time = new Date()
+// let hour =(23)
+// let minute =(59)
+// let second =(40)
+// setInterval(() => {
+//     console.log(hour,minute,second);
+//     second++
+//     if(second>59){
+//         second=0
+//         minute++
+//     }
+//     if(minute>59){
+//         minute=0
+//         hour++
+    
+//     if (hour>23){
+//         hour=0
+//     }
+
+//     }
+
+    
+// }, 1000);
+// function suprize(str){
+//     console.log(str);
+//     console.log(str);
+//     console.log(str);
+//     console.log(str);
+//     console.log(str);
+//     console.log(str);
+//     console.log(str);
+//     console.log(str);
+//     console.log(str);
+//     console.log(str);
+// }
+// suprize("Hello bro ");
+// suprize("Hello browdy ");
+// suprize("Hello brosky ");
+// function suprize(c=2,d=3){
+//     console.log(c+d)
+
+       
+//   setInterval(() => {
+//       let a, b 
+//       a=Math.random()*100
+//       b=Math.random()*100
+//       suprize(a,b)
+//   }, 0.00001);
+// function toUpper(str=""){
+//     return str.toLocaleUpperCase()
+// }
+// let news = toUpper("gdfgsdbvfdsgfdgsdf")
+// console.log(news)
+
+
+//     let Name1 = function(str=""){
+//     return str.toLocaleUpperCase()
+//     }
+//     let mem =Name1("sdlfmdvoijdfvomd")
+//     console.log(mem)
+//     let _Name2 = (str="") =>{
+//         return str.toLocaleUpperCase()
+//     }
+//     let mems = _Name2("svdvererrc")
+//     console.log(mems)
+// let times = () => {
+//     setInterval(() =>{
+//     let time= new Date()
+//     console.log(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
+//     }, 1000)
+// }
+// times()
+// function suprize(c=2,d=3){
+//      console.log(a+b)
+// }
+// function both(){
+//     setTimeout(() =>{
+    
+//     },1000)
+// }
+// both()
+// let a=2,b=3;
+// let text="";
+// a>b?(text=`$(a)>$(b)`):a==b?(text=`$(a)<$(b) `):(text=`$(a)>$(b)` );
+// console.log(text);
+// let time = new Date;
+// let day = time.getDay();
+// console.log(day)
+// switch(day){
+//     case 1: console.log("Monday"); break;
+//     case 2: console.log("Tuesday"); break;
+//     case 3: console.log("Wednesday"); break;
+//     case 4: console.log("Thursday"); break;
+//     case 5: console.log("Friday"); break;
+//     case 6: console.log("Saturday"); break;
+//     case 7: console.log("Sunday"); break;
+//     default:console.log("None day"); break;
+// }
+// function EkiXanali(ab){
+//     let a= Math.floor(ab/10);
+//     let b=(ab%10); 
+//     let text= ""
+    
+//     switch(a){
+//         case 1: text=" on";break;
+//         case 2: text=" jigirma";break;
+//         case 3: text=" otiz";break;
+//         case 4: text=" qiriq";break;
+//         case 5: text=" eliw";break;
+//         case 6: text=" altpis";break;
+//         case 7: text=" jetpis";break;
+//         case 8: text=" seksen";break;
+//         case 9: text=" toqsan";break;
+
+//     }
+
+//     switch(b){
+//         case 1: text+=" bir";break;
+//         case 2: text+=" eki";break;
+//         case 3: text+=" ush";break;
+//         case 4: text+=" tort";break;
+//         case 5: text+=" bes";break;
+//         case 6: text+=" alti";break;
+//         case 7: text+=" jetti";break;
+//         case 8: text+=" segiz";break;
+//         case 9: text+=" togiz";break;
+//     }
+ 
+// console.log(text)
+// }
+// EkiXanali(66)
+// function Algoritm(a,b, action){
+//     switch(action){
+//         case "1": console.log(a+b);break;
+//         case "2": console.log(a-b);break;
+//         case "3": console.log(a*b);break;
+//         case "4": console.log(a/b);break;
+
+//     }
+// }
+// Algoritm(10,45, "1" )
+// Algoritm(10,45, "2" )
+// Algoritm(10,45, "3" )
+// Algoritm(10,45, "4" )
+// function eror(){
+//     const pi = Math.PI;
+//     console.log(pi);
+//     pi = 2;
+// }
+// try{
+//   eror();
+// } catch (eror){
+//     console.log(eror);
+// }
+// function add(){
+//     console.log()
+// }
+// for (let i=0; i<=100; i++){
+//     console.log(i);
+// }
+// for (let i=0; i<=10; i++){
+//     console.log(i);
+// }
+
+
+// let text= "Happy new year";
+// for (let i=0;i<=14 - 1;i++){
+//     console.log(text[i]);
+// }
+// function cool1(){
+//     let a=32,b=33;
+//     console.log(a+b);
+// }
+// cool1()
+// const cool2=(n)=>{
+// console.log(n);
+// }
+// cool2(34)
+// const cool3 = function(a){
+//     alert(a)
+// }
+// cool3("hello")
+// let data =[1,2,32,45,45,54];
+// let prt =Array(34,55,66,77,88,99,72);
+// console.log(data);
+// console.log(prt);
+// let ner=data.concat(prt);
+// data.push(12,32,45);
+// data.unshift(0,0,0);
+// data.shift(8,8,8);
+// console.log(data);
